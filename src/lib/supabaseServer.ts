717c23075cdf6629a3cf2b5b7a32b400a813cc81
Glitch199818 <sushruthidg10@gmail.com>
2025-12-30 23:20:25 +0530

    Fix TypeScript error in supabaseServer

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -16,7 +16,7 @@ export const createSupabaseRouteHandlerClient = async () => {
   return createServerClient<Database>(supabaseUrl, supabaseAnonKey, {
     cookies: {
       getAll: () => cookieStore.getAll(),
-      setAll: (cookiesToSet) => {
+      setAll: (cookiesToSet: Array<{ name: string; value: string; options?: any }>) => {
         cookiesToSet.forEach(({ name, value, options }) => {
           cookieStore.set(name, value, options);
         });
